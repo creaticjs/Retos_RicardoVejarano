503b5ea4946f38b2589c52ec96e4eb50fddc0b86
Ricardo <ricardo.vejarano.garcia@gmail.com>
2019-07-27 11:55:29 -0500

    Change last URL ref

--- reto_2/global.js
+++ reto_2/global.js
@@ -1,41 +1,41 @@
 function navegate(valueNavegate) {
     console.log(valueNavegate);
     switch (valueNavegate) {
-        case 1: window.location.replace('https://creaticjs.github.io/Retos_RicardoVejarano/reto_2_1/index.html');
+        case 1: window.location.replace('https://creaticjs.github.io/Retos_RicardoVejarano/reto_2/reto_2_1/index.html');
             break;
-        case 2: window.location.replace('https://creaticjs.github.io/Retos_RicardoVejarano/reto_2_2/problem2.html')
+        case 2: window.location.replace('https://creaticjs.github.io/Retos_RicardoVejarano/reto_2/reto_2_2/problem2.html')
             break;
-        case 3: window.location.replace('https://creaticjs.github.io/Retos_RicardoVejarano/reto_2_3/problem3.html')
+        case 3: window.location.replace('https://creaticjs.github.io/Retos_RicardoVejarano/reto_2/reto_2_3/problem3.html')
             break;
-        case 4: window.location.replace('https://creaticjs.github.io/Retos_RicardoVejarano/reto_2_4/problem4.html')
+        case 4: window.location.replace('https://creaticjs.github.io/Retos_RicardoVejarano/reto_2/reto_2_4/problem4.html')
             break;
-        case 5: window.location.replace('https://creaticjs.github.io/Retos_RicardoVejarano/reto_2_5/problem5.html')
+        case 5: window.location.replace('https://creaticjs.github.io/Retos_RicardoVejarano/reto_2/reto_2_5/problem5.html')
             break;
-        case 6: window.location.replace('https://creaticjs.github.io/Retos_RicardoVejarano/reto_2_6/problem6.html')
+        case 6: window.location.replace('https://creaticjs.github.io/Retos_RicardoVejarano/reto_2/reto_2_6/problem6.html')
             break;
-        case 7: window.location.replace('https://creaticjs.github.io/Retos_RicardoVejarano/reto_2_7/problem7.html')
+        case 7: window.location.replace('https://creaticjs.github.io/Retos_RicardoVejarano/reto_2/reto_2_7/problem7.html')
             break;
-        case 8: window.location.replace('https://creaticjs.github.io/Retos_RicardoVejarano/reto_2_8/problem8.html')
+        case 8: window.location.replace('https://creaticjs.github.io/Retos_RicardoVejarano/reto_2/reto_2_8/problem8.html')
             break;
-        case 9: window.location.replace('https://creaticjs.github.io/Retos_RicardoVejarano/reto_2_9/problem9.html')
+        case 9: window.location.replace('https://creaticjs.github.io/Retos_RicardoVejarano/reto_2/reto_2_9/problem9.html')
             break;
-        case 10: window.location.replace('https://creaticjs.github.io/Retos_RicardoVejarano/reto_2_10/problem10.html')
+        case 10: window.location.replace('https://creaticjs.github.io/Retos_RicardoVejarano/reto_2/reto_2_10/problem10.html')
             break;
-        case 11: window.location.replace('https://creaticjs.github.io/Retos_RicardoVejarano/reto_2_11/problem11.html')
+        case 11: window.location.replace('https://creaticjs.github.io/Retos_RicardoVejarano/reto_2/reto_2_11/problem11.html')
             break;
-        case 12: window.location.replace('https://creaticjs.github.io/Retos_RicardoVejarano/reto_2_12/problem12.html')
+        case 12: window.location.replace('https://creaticjs.github.io/Retos_RicardoVejarano/reto_2/reto_2_12/problem12.html')
             break;
-        case 13: window.location.replace('https://creaticjs.github.io/Retos_RicardoVejarano/reto_2_13/problem13.html')
+        case 13: window.location.replace('https://creaticjs.github.io/Retos_RicardoVejarano/reto_2/reto_2_13/problem13.html')
             break;
-        case 14: window.location.replace('https://creaticjs.github.io/Retos_RicardoVejarano/reto_2_14/problem14.html')
+        case 14: window.location.replace('https://creaticjs.github.io/Retos_RicardoVejarano/reto_2/reto_2_14/problem14.html')
             break;
-        case 15: window.location.replace('https://creaticjs.github.io/Retos_RicardoVejarano/reto_2_15/problem15.html')
+        case 15: window.location.replace('https://creaticjs.github.io/Retos_RicardoVejarano/reto_2/reto_2_15/problem15.html')
             break;
-        case 16: window.location.replace('https://creaticjs.github.io/Retos_RicardoVejarano/reto_2_16/problem16.html')
+        case 16: window.location.replace('https://creaticjs.github.io/Retos_RicardoVejarano/reto_2/reto_2_16/problem16.html')
             break;
-        case 17: window.location.replace('https://creaticjs.github.io/Retos_RicardoVejarano/reto_2_17/problem17.html')
+        case 17: window.location.replace('https://creaticjs.github.io/Retos_RicardoVejarano/reto_2/reto_2_17/problem17.html')
             break;
-        case 18: window.location.replace('https://creaticjs.github.io/Retos_RicardoVejarano/reto_2_18/problem18.html')
+        case 18: window.location.replace('https://creaticjs.github.io/Retos_RicardoVejarano/reto_2/reto_2_18/problem18.html')
             break;
     }
 }
